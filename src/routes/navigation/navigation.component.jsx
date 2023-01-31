2dea65638a6bbae77da79aba4424c96c7b19202b
Abdul Mubeen <abdulmubeen57@gmail.com>
2023-01-31 12:03:43 +0530

    Update navigation.component.jsx

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -23,7 +23,8 @@ const NavigationBar = () => {
           </Link>
           {currentUser ? (
             <span className="nav-link" onClick={signOutUser}>
-              SIGN OUT
+              {" "}
+              SIGN OUT{" "}
             </span>
           ) : (
             <Link className="nav-link" to="/auth">
